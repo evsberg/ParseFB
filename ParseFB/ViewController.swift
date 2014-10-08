f263a88fffc059c301ba665935c450eb2cfe9474
evsberg <evangapella@gmail.com>
2014-10-08 14:30:53 +1100

    Initial Commit

---
+++ ParseFB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParseFB
+//
+//  Created by Evan Gapella on 8/10/2014.
+//  Copyright (c) 2014 Evan Gapella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
